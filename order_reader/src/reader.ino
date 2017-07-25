a370c9427708fa92ef53e936aa319d98981dce50
peter <peter.bennett@embecosm.com>
2017-07-25 15:53:25 +0100

    Full gradient based reader code now featuring arrays which are functional but the whole code no longer is

--- order_reader/src/reader.ino
+++ order_reader/src/reader.ino
@@ -1,41 +1,19 @@
 /*
 The code to work the EDSAC simulating tape reader stop waiting for me dan
  */
+#include <math.h>
 
-int sensorPin1  = A4;    // select the input pin for the potentiometer
-int sensorPin2  = A3;    // select the input pin for the potentiometer
-int sensorPin4  = A2;    // select the input pin for the potentiometer
-int sensorPin8  = A1;    // select the input pin for the potentiometer
-int sensorPin16 = A0;    // select the input pin for the potentiometer
+int sensorPin[05] = {A4,A3,A2,A1,A0};
 int ledPin = 13;      // select the pin for the LED
-int sensorValue1  = 0;  // variable to store the Value coming from the sensor
-int sensorValue2  = 0;  // variable to store the Value coming from the sensor
-int sensorValue4  = 0;  // variable to store the Value coming from the sensor
-int sensorValue8  = 0;  // variable to store the Value coming from the sensor
-int sensorValue16 = 0;  // variable to store the Value coming from the sensor
-int olderValue1  = 0;  // variable to store the Value coming from the sensor
-int olderValue2  = 0;  // variable to store the Value coming from the sensor
-int olderValue4  = 0;  // variable to store the Value coming from the sensor
-int olderValue8  = 0;  // variable to store the Value coming from the sensor
-int olderValue16 = 0;  // variable to store the Value coming from the sensor
-int oldestValue1  = 0;  // variable to store the Value coming from the sensor
-int oldestValue2  = 0;  // variable to store the Value coming from the sensor
-int oldestValue4  = 0;  // variable to store the Value coming from the sensor
-int oldestValue8  = 0;  // variable to store the Value coming from the sensor
-int oldestValue16 = 0;  // variable to store the Value coming from the sensor
-int bit1  = 0;
-int bit2  = 0;
-int bit4  = 0;
-int bit8  = 0;
-int bit16 = 0;
-int bitVal1  = 0;
-int bitVal2  = 0;
-int bitVal4  = 0;
-int bitVal8  = 0;
-int bitVal16 = 0;
-int olderResponse = 0;
-int oldestResponse = 0;
-int currentResponse = 0;
+int sensorValue[05];
+int oldValue[05];
+int olderValue[05];
+int oldestValue[05];
+int bits[05];
+double bitsValue[05];
+double olderResponse = 0;
+double oldestResponse = 0;
+double currentResponse = 0;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -45,182 +23,69 @@ void setup() {
 
 void loop() {
   // shift everything about:
+  int i = 0;
+  int j = 0;
   oldestResponse = olderResponse;
   olderResponse = currentResponse;
-  oldestValue1  = olderValue1;
-  oldestValue2  = olderValue2;
-  oldestValue4  = olderValue4;
-  oldestValue8  = olderValue8;
-  oldestValue16 = olderValue16;
-  olderValue1  = sensorValue1;
-  olderValue2  = sensorValue2;
-  olderValue4  = sensorValue4;
-  olderValue8  = sensorValue8;
-  olderValue16 = sensorValue16;
-  sensorValue1  = analogRead(sensorPin1);
-  sensorValue2  = analogRead(sensorPin2);
-  sensorValue4  = analogRead(sensorPin4);
-  sensorValue8  = analogRead(sensorPin8);
-  sensorValue16 = analogRead(sensorPin16); 
+  for (i = 0; i < 5; i++){
+    oldestValue[i] = olderValue[i];
+    olderValue[i]  = oldValue[i];
+    oldValue[i] = sensorValue[i];
+    sensorValue[i] = analogRead(sensorPin[i]);
+  };
   //make any changes to the bits
-  if(sensorValue1 < oldestValue1 - 20){
-    bit1 = 1;
-    bitVal1 = 1;
+  for (j = 0; j < 5; j++){
+    if(sensorValue[j] < oldestValue[j] - 25){
+      bits[j] = 1;
+      bitsValue[j] = pow( 2 ,  j);
+    }
+    else if(sensorValue[j] < oldestValue[j] + 25){
+      bits[j] = 0;
+      bitsValue[j] = 0;
+    };
   }
-  else if (sensorValue1 > oldestValue1 + 20){
-    bit1 = 0;
-    bitVal1 = 0;
-  };
-  if(sensorValue2 < oldestValue2 - 20){
-    bit2 = 1;
-    bitVal2 = 2;
-  }
-  else if (sensorValue2 > oldestValue2 + 20){
-    bit2 = 0;
-    bitVal2 = 0;
-  };
-  if(sensorValue4 < oldestValue4 - 20){
-    bit4 = 1;
-    bitVal4 = 4;
-  }
-  else if (sensorValue4 > oldestValue4 + 20){
-    bit4 = 0;
-    bitVal4 = 0;
-  };
-  if(sensorValue8 < oldestValue8 - 20){
-    bit8 = 1;
-    bitVal8 = 8;
-  }
-  else if (sensorValue8 > oldestValue8 + 20){
-    bit8 = 0;
-    bitVal8 = 0;
-  };
-  if(sensorValue16 < oldestValue16 - 20){
-    bit16 = 1;
-    bitVal16 = 16;
-  }
-  else if (sensorValue16 > oldestValue16 + 20){
-    bit16 = 0;
-    bitVal16 = 0;
-  };
-  currentResponse = bitVal1 + bitVal2 + bitVal4 + bitVal8 + bitVal16;
+
+  currentResponse = bitsValue[0] + bitsValue[1] + bitsValue[2] + bitsValue[3] + bitsValue[4];
   if(currentResponse == olderResponse){
   }
   else{
     Serial.print("Binary: ");
-    Serial.print(bit16);
-    Serial.print(bit8);
-    Serial.print(bit4);
-    Serial.print(bit2);
-    Serial.print(bit1);
+    Serial.print(bits[4]);
+    Serial.print(bits[3]);
+    Serial.print(bits[2]);
+    Serial.print(bits[1]);
+    Serial.print(bits[0]);
     Serial.print("    Actual value: ");
-    if(bitVal16 + bitVal8 + bitVal4 + bitVal2 + bitVal1 < 10){
+    if(currentResponse < 10){
       Serial.print(" ");
     };
-    Serial.print(bitVal16 + bitVal8 + bitVal4 + bitVal2 + bitVal1);
+    Serial.print(currentResponse);
     Serial.print("    Differences: ");
-    if (sensorValue16 - oldestValue16 < -100){
-      Serial.print(" ");
-    }
-    else if (sensorValue16 - oldestValue16 < -10){
-      Serial.print("  ");
-    }
-    else if (sensorValue16 - oldestValue16 < 0){
-      Serial.print("   ");
-    }
-    else if (sensorValue16 - oldestValue16 < 10){
-      Serial.print("    ");
-    }
-    else if (sensorValue16 - oldestValue16 < 100){
-      Serial.print("   ");
-    }
-    else{
-      Serial.print("  ");
+    int k = 0;
+    for (k=0; k<5; k++){
+      if (sensorValue[k] - oldestValue[k] < -99.5){
+        Serial.print(" ");
+      }
+      else if (sensorValue[k] - oldestValue[k] < -9.5){
+        Serial.print("  ");
+      }
+      else if (sensorValue[k] - oldestValue[k] < -0.5){
+        Serial.print("   ");
+      }
+      else if (sensorValue[k] - oldestValue[k] < 9.5){
+        Serial.print("    ");
+      }
+      else if (sensorValue[k] - oldestValue[k] < 99.5){
+        Serial.print("   ");
+      }
+      else{
+        Serial.print("  ");
+      };
+      Serial.print(sensorValue[k] - oldestValue[k]);
     };
-    Serial.print(sensorValue16 - oldestValue16);
-    Serial.print(" ");  
-    if (sensorValue8 - oldestValue8 < -100){
-      Serial.print(" ");
-    }
-    else if (sensorValue8 - oldestValue8 < -10){
-      Serial.print("  ");
-    }
-    else if (sensorValue8 - oldestValue8 < 0){
-      Serial.print("   ");
-    }
-    else if (sensorValue8 - oldestValue8 < 10){
-      Serial.print("    ");
-    }
-    else if (sensorValue8 - oldestValue8 < 100){
-      Serial.print("   ");
-    }
-    else{
-      Serial.print("  ");
-    };
-    Serial.print(sensorValue8 - oldestValue8);
-    Serial.print(" ");  
-    if (sensorValue4 - oldestValue4 < -100){
-      Serial.print(" ");
-    }
-    else if (sensorValue4 - oldestValue4 < -10){
-      Serial.print("  ");
-    }
-    else if (sensorValue4 - oldestValue4 < 0){
-      Serial.print("   ");
-    }
-    else if (sensorValue4 - oldestValue4 < 10){
-      Serial.print("    ");
-    }
-    else if (sensorValue4 - oldestValue4 < 100){
-      Serial.print("   ");
-    }
-    else{
-      Serial.print("  ");
-    };
-    Serial.print(sensorValue4 - oldestValue4);
-    Serial.print(" ");
-    if (sensorValue2 - oldestValue2 < -100){
-      Serial.print(" ");
-    }
-    else if (sensorValue2 - oldestValue2 < -10){
-      Serial.print("  ");
-    }
-    else if (sensorValue2 - oldestValue2 < 0){
-      Serial.print("   ");
-    }
-    else if (sensorValue2 - oldestValue2 < 10){
-      Serial.print("    ");
-    }
-    else if (sensorValue2 - oldestValue2 < 100){
-      Serial.print("   ");
-    }
-    else{
-      Serial.print("  ");
-    };
-    Serial.print(sensorValue2 - oldestValue2);
-    Serial.print(" ");  
-    if (sensorValue1 - oldestValue1 < -100){
-      Serial.print(" ");
-    }
-    else if (sensorValue1 - oldestValue1 < -10){
-      Serial.print("  ");
-    }
-    else if (sensorValue1 - oldestValue1 < 0){
-      Serial.print("   ");
-    }
-    else if (sensorValue1 - oldestValue1 < 10){
-      Serial.print("    ");
-    }
-    else if (sensorValue1 - oldestValue1 < 100){
-      Serial.print("   ");
-    }
-    else{
-      Serial.print("  ");
-    };
-    Serial.print(sensorValue1 - oldestValue1);
     Serial.println();
   };
   // turn the ledPin on
   digitalWrite(ledPin, HIGH);  
-  delay(20);                  
+  delay(30);                  
 }
