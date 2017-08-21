5fb0118bb4208f5791c715d0bf22f584597711c4
Dan Gorringe <dan.gorringe@hotmail.co.uk>
2017-08-21 16:37:25 +0100

    Added button functionality

--- initial_orders/src/initial_orders.ino
+++ initial_orders/src/initial_orders.ino
@@ -16,8 +16,8 @@
 
 Adafruit_MCP23017 mcp2;
 Adafruit_MCP23017 mcp1;
-  
-void setup() {  
+
+void setup() {
   mcp1.begin(1);      // use default address 0
   mcp2.begin(0);      // use default address 0
   mcp1.pinMode( 0, INPUT);
@@ -38,31 +38,71 @@ void setup() {
   mcp1.pinMode(15, INPUT);
   mcp2.pinMode( 2, INPUT);
   pinMode(13, OUTPUT);  // use the p13 LED as debugging
+  pinMode(12, OUTPUT);
   Serial.begin(9600);
+  mcp2.pinMode( 5, INPUT);
 }
 
-
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current s'nOk skaaskskdyjrtrgotr Therefore thtate of the button
+int lastButtonState = 0;     // previous state of the button
+long savedOrders[40];
+int x = 1;
 
 void loop() {
+
   // The LED will 'echo' the button
   digitalWrite(13, mcp2.digitalRead(2));
-  Serial.print(mcp1.digitalRead(8));
-  Serial.print(mcp1.digitalRead(9));
-  Serial.print(mcp1.digitalRead(10));
-  Serial.print(mcp1.digitalRead(11));
-  Serial.print(mcp1.digitalRead(12));
-  Serial.print(mcp1.digitalRead(13));
-  Serial.print(mcp1.digitalRead(14));
-  Serial.print(mcp1.digitalRead(15));
-  Serial.print(mcp1.digitalRead(0));
-  Serial.print(mcp1.digitalRead(1));
-  Serial.print(mcp1.digitalRead(2));
-  Serial.print(mcp1.digitalRead(3));
-  Serial.print(mcp1.digitalRead(4));
-  Serial.print(mcp1.digitalRead(5));
-  Serial.print(mcp1.digitalRead(6));
-  Serial.print(mcp1.digitalRead(7));
-  Serial.print(mcp2.digitalRead(2));
-  Serial.println();
-  delay(100);
+  digitalWrite(12, mcp1.digitalRead(7));
+
+  buttonState = mcp2.digitalRead(5);
+
+  if (buttonState != lastButtonState){    // If far right button is pressed then serial print order, increment counter, print program counter and print decimal order
+    if (buttonState == HIGH) {
+      buttonPushCounter++ ;
+      Serial.print(mcp1.digitalRead(8));
+      Serial.print(mcp1.digitalRead(9));
+      Serial.print(mcp1.digitalRead(10));
+      Serial.print(mcp1.digitalRead(11));
+      Serial.print(mcp1.digitalRead(12));
+      Serial.print(mcp1.digitalRead(13));
+      Serial.print(mcp1.digitalRead(14));
+      Serial.print(mcp1.digitalRead(15));
+      Serial.print(mcp1.digitalRead(0));
+      Serial.print(mcp1.digitalRead(1));
+      Serial.print(mcp1.digitalRead(2));
+      Serial.print(mcp1.digitalRead(3));
+      Serial.print(mcp1.digitalRead(4));
+      Serial.print(mcp1.digitalRead(5));
+      Serial.print(mcp1.digitalRead(6));
+      Serial.print(mcp1.digitalRead(7));
+      Serial.print(mcp2.digitalRead(2));
+      Serial.println();
+      savedOrders[x] =  mcp2.digitalRead(2)*1 +
+                        mcp1.digitalRead(7)*2 +
+                        mcp1.digitalRead(6)*4 +
+                        mcp1.digitalRead(5)*8 +
+                        mcp1.digitalRead(4)*16 +
+                        mcp1.digitalRead(3)*32 +
+                        mcp1.digitalRead(2)*64 +
+                        mcp1.digitalRead(1)*128 +
+                        mcp1.digitalRead(0)*256 +
+                        mcp1.digitalRead(15)*512 +
+                        mcp1.digitalRead(14)*1024 +
+                        mcp1.digitalRead(13)*2048 +
+                        mcp1.digitalRead(12)*4096 +
+                        mcp1.digitalRead(11)*8192 +
+                        mcp1.digitalRead(10)*16384 +
+                        mcp1.digitalRead(9)*32768 +
+                        mcp1.digitalRead(8)*65536;
+
+      Serial.print(savedOrders[x]);
+      Serial.println();
+      Serial.print(x);
+      Serial.println();
+      x++ ;
+      delay(100);
+    }
+  }
+  lastButtonState = buttonState;
 }
