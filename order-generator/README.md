03619a0a060d3417accdde6c9e06000a23e91bd5
Jeremy Bennett <jeremy.bennett@embecosm.com>
2017-08-31 20:12:14 +0100

    Complete rewrite of the order-generator tool
    	This tool is used to generate paper tape using a thermal printer.
    
    Files changed:
    
    	* order-generator/README.md: Updated with instructions for new program.
    	* order-generator/genmap.c: Created.
    	* order-generator/lib/readme.txt: Deleted.
    	* order-generator/platformio.ini: Deleted.
    	* order-generator/printcode/orders.txt: Moved from src directory.
    	* order-generator/printcode/printcode.ino: Moved from src
    	directory and completely rewritten.

--- order-generator/README.md
+++ order-generator/README.md
@@ -1,6 +1,42 @@
 # Order Generator
-To connect over serial with an arduino uno (clone) set up with the thermal printer
-[in the default way.](https://learn.adafruit.com/mini-thermal-receipt-printer/microcontroller).
-Will print the first 5 bits of any number you send it.
 
-9600 Baud rate.
+An Arduino program to generate 5-hole paper tape using an Adafruit thermal
+printer.
+
+## Prequisites
+
+This uses the
+[Adafruit Thermal Printer library](https://github.com/adafruit/Adafruit-Thermal-Printer-Library). Clone
+this into your Arduino library directory:
+- On Linux: (home directory)/sketchbook/libraries
+- On Mac:: In (home directory)/Documents/Arduino/Libraries
+- On Windows:: My Documents\Arduino\libraries
+
+For example on Linux
+```
+cd ~/sketchbook/libraries
+git clone https://github.com/adafruit/Adafruit-Thermal-Printer-Library.git Adafruit_Thermal
+```
+You will need to restart the Arduino IDE after setting this up.
+
+## The program
+
+Load `printcode.ino` into the Arduino IDE and upload to the printer.
+
+Open a serial monitor and enter numbers (in decimal) to be uploaded, with each
+number followed by a comma.
+
+The numbers will be printed in 5-hole format. Note that only the bottom 5 bits
+of numbers larger than 31 will be printed.
+
+
+## The generator program
+
+The images for the data and sprocket holes are held as constant bitmaps in
+`printcode.ino`. The declarations and initialization of these can be generated
+on your host computer using the `gencode.c` program.  For example on Linux.
+```
+gcc -o genmap genmap.c
+./genmap holeMap 50 50
+./genmap holeMap 50 35
+```
---
+++ order-generator/genmap.c
@@ -0,0 +1,101 @@
+// Map generator for thermal printer
+
+// Copyright (C) 2017 Embecosm Limited <www.embecosm.com>
+
+// Contributor: Peter Bennett <peter.bennett@embecosm.com>
+// Contributor: Dan Gorringe <dan.gorringe@embecosm.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Build this program on your host machine, to create the declaration for an
+// image map of a black circle within a square.
+
+// These have to be constant static declarations, so they end up in Flash, not
+// RAM in the Arduino.
+
+// Takes three arguments in position order
+
+// - name of the map (C variable name)
+// - size of the square (pixels)
+// - diameter of the hole.
+
+// Error checking is minimal.
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int
+main (int   argc,
+      char *argv[])
+{
+  char *varname = argv[1];
+  int  width = atoi (argv[2]);
+  int  diam = atoi (argv[3]);
+  int  r = diam / 2;
+  int  r_sq = r * r;
+
+  int  i, x, y;
+  uint8_t pixmap[50000];		// Just loads
+  int map_bytes = (width * width + 7) / 8;
+
+  memset (pixmap, 0, map_bytes);     // Clear all the bits
+
+  for (x = 0; x < width; x++)
+    {
+      int  x_off    = x - width / 2;
+      int  x_off_sq = x_off * x_off;
+
+      for (int y = 0; y < width; y++)
+	{
+	  int  y_off = y - width / 2;
+	  int  y_off_sq = y_off * y_off;
+
+	  int rowbit  = y * width + x;
+	  int rowbyte = rowbit / 8;
+	  int bit     = 7 - rowbit % 8;
+
+	  if ((x_off_sq + y_off_sq) < r_sq)
+	    {
+	      // In circle - bit is black.
+	      pixmap[rowbyte] |= 1 << bit;
+	    }
+	  else
+	    {
+	      // Not in circle - bit is white
+	      pixmap[rowbyte] &= ~(1 << bit);
+	    }
+	}
+    }
+
+  printf ("static const uint8_t  %s[%d] = {", varname, map_bytes);
+
+  for (i = 0; i < map_bytes; i++)
+    {
+      if (0 == i % 8)
+	printf ("\n  ");
+      else
+	printf (" ");
+
+      printf ("0x%02x", pixmap[i]);
+
+      if (i < (map_bytes - 1))
+	printf (",");
+    }
+
+  printf ("\n};\n");
+
+}
