aec61d5c526d9bdf1a7a52e714977159a5799e35
Jeremy Bennett <jeremy.bennett@embecosm.com>
2017-09-03 18:46:38 +0100

    First attempt at fully working paper tape reader.
    Files changed:
    
    	* reader.ino: Rewritten to use second derivatives to find edges.

--- papertape-reader/code/reader/reader.ino
+++ papertape-reader/code/reader/reader.ino
@@ -22,15 +22,11 @@
 
 static const int NUM_BITS = 5;
 
-// How many value to aggregate
-
-static const int NUM_VALS = 10;
-
 // All the LED sensors
 
 static const int sensorPin[NUM_BITS] = {
-  A5, A4, A2, A1, A0 };
-static const int sprocketPin = A3;
+  A5, A4, A3, A1, A0 };
+static const int sprocketPin = A2;
 
 // All 6 reader LEDs are driven off the same pin.
 
@@ -40,36 +36,64 @@ static const int readerLedPin = 13;      // select the pin for the LED
 
 static const int statusLedPin = 12;
 
-// Record of the last 20 sensor and sprocket values
+// Number of values to average forwards and backwards.
 
-int sensorValue[NUM_VALS][NUM_BITS];
-int sprocketValue[NUM_VALS];
+static const int NUM_VALS = 5;
 
-// Which is the next set of data to be collected (range 0 to NUM_VALS - 1)
+// Number of samples to aggregate to be able to compute a central second
+// derivative. We smooth NUM_VALS each side of the raw data to get the
+// derivative. No further smoothing after that.
 
-int nextData;
+static const int NUM_SAMPLES = NUM_VALS * 2 + 1;
 
-// State of pins
+// Structure of a sensor value
 
-uint8_t sensorState[NUM_BITS];
-uint8_t sprocketState;
+struct sensorData
+{
+  int vals[NUM_SAMPLES];
+  int gradients[NUM_SAMPLES];
+  int gradient2;
+  int gradient2prev;
+};
 
-// Threshold for slope
+// Record of sensor and sprocket values
 
-static const int THRESHOLD = 20;
+struct sensorData  dataInfo[NUM_BITS];
+struct sensorData  sprocketInfo;
+
+// Which is the next set of data to be collected (range 0 to NUM_SAMPLES - 1)
+
+static int nextData;
+
+// True if we are in a sprocket hole
+
+static bool sprocketState;
+
+// Threshold for sprocket state change. First derivative must be more than
+// this.
+
+static const int SPROCKET_THRESHOLD = 20;
+
+// State of pins. True if we are in a hole
+
+static bool sensorState[NUM_BITS];
+
+// Threshold for sensor state change. First derivative must be more than
+// this.
+
+static const int SENSOR_THRESHOLD = 20;
+
+// The data we read;
+
+static uint8_t data;
+
+// Maximum abount of debug
+
+static const int DBG_MAX = 1500;
 
 // Debug counter
 
-static const int DBG_CNT = 250;
-
-struct
-{
-  int  current;
-  int  oldest;
-  int  gradient;
-} debugData[DBG_CNT];
-
-int debugCount;
+int dbgCnt;
 
 //! Standard setup
 
@@ -84,38 +108,84 @@ void setup()
 
   // Make sure we can talk to the serial monitor
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  // Populate our initial data
+  // Debug setup
+
+  dbgCnt = 0;
+  Serial.println ("Get ready");
+  delay (2000);
+
+  // Turn on the LEDs and allow resistors to respond.
+
+  digitalWrite (readerLedPin, HIGH);
+  digitalWrite (statusLedPin, LOW);
+  delay (500);
+
+  // Populate our initial data.  Need to do this twice to ensure the first
+  // derivative values are meaningful.
 
   nextData = 0;
 
   do
     {
-      sprocketValue[nextData] = analogRead (sprocketPin);
+      sprocketInfo.vals[nextData] = analogRead (sprocketPin);
 
       for (int i = 0; i < NUM_BITS; i++)
-        sensorValue[nextData][i] = analogRead (sensorPin[i]);
+        dataInfo[i].vals[nextData] = analogRead (sensorPin[i]);
 
-      nextData = (nextData == (NUM_VALS - 1)) ? 0 : nextData + 1;
+      nextData = (nextData + 1) % NUM_SAMPLES;
     }
   while (nextData != 0);
 
-  // Set up state. Assume all LOW (FALSE) to start with.
+  // Second time round, populate the gradients
 
-  sprocketState = 0;
+  do
+    {
+      int tot = 0;
+      int nextGrad = (nextData - NUM_VALS + 1 + NUM_SAMPLES) % NUM_SAMPLES;
+
+      sprocketInfo.vals[nextData] = analogRead (sprocketPin);
+
+      for (int j = nextGrad - NUM_VALS; j < nextGrad; j++)
+        {
+          int prev = (j + NUM_SAMPLES) % NUM_SAMPLES;
+          int curr = (j + NUM_VALS) % NUM_SAMPLES;
+          tot += sprocketInfo.vals[curr] - sprocketInfo.vals[prev];
+        }
+
+      sprocketInfo.gradients[nextGrad] = tot;
+
+      for (int i = 0; i < NUM_BITS; i++)
+        {
+          tot = 0;
+          dataInfo[i].vals[nextData] = analogRead (sensorPin[i]);
+
+          for (int j = nextGrad - NUM_VALS; j < nextGrad; j++)
+            {
+              int prev = (j + NUM_SAMPLES) % NUM_SAMPLES;
+              int curr = (j + NUM_VALS) % NUM_SAMPLES;
+              tot += dataInfo[i].vals[curr] - dataInfo[i].vals[prev];
+            }
+
+          dataInfo[i].gradients[nextGrad] = tot;
+        }
+
+      nextData = (nextData + 1) % NUM_VALS;
+    }
+  while (nextData != 0);
+
+  // Assume not in sprocket hole or data hole
+
+  sprocketState = false;
 
   for (int i = 0; i < NUM_BITS; i++)
-    sensorState[i] = 0;
+    sensorState[i] = false;
 
-  // Turn on the LEDs
+  // No data read yet
 
-  digitalWrite (readerLedPin, HIGH);
-  digitalWrite (statusLedPin, LOW);
+  data = 0;
 
-  debugCount = 0;
-  Serial.println ("Get ready");
-  delay (2000);
   Serial.println ("Go");
   delay (500);
 
@@ -124,89 +194,139 @@ void setup()
 
 //! Main loop
 
+//! The sensor is a light dependent resistor (LDR). The LDR resistance varies
+//! from 10s of kOhm in full light to 20MOhm in the dark.  The LDR is
+//! connected to the 5V rail and then via a 22kOhm resistor to ground, with
+//! the value read at the connection between the LDR and resistor.
+
+//! So the more light the LDR receives, the higher the read value.  Since
+//! holes in the paper tape are represented as dark spots, this means minima
+//! correspond to holes.
+
+//! We calculate mid-point central first and second derivatives for the current
+//! reading of the sprocket hole. When the second deriviative is zero, we
+//! treat this as the edge of a sprocket hole. If the first derivative is
+//! positive we are approaching a minimum (i.e. entering the sprocket hole)
+//! and if it is negative we are approaching a maximum (i.e. leaving the
+//! sprocket hole).
+
+//! During the
+
 //! @note Don't go for GNU style declaration here - it breaks Arduino
 
 void loop()
 {
   delay (5);
 
-  // Capture gradients
+  // Capture data and compute second derivatives
 
-  int dataGradient[NUM_BITS];
-  int sprocketGradient;
+  int nextGrad = (nextData - NUM_VALS + 1 + NUM_SAMPLES) % NUM_SAMPLES;
+  int prevGrad = (nextGrad - 1 + NUM_SAMPLES) % NUM_SAMPLES;
+  int tot = 0;
 
-  int oldestData = (nextData + 1) % NUM_VALS;
+  sprocketInfo.vals[nextData] = analogRead (sprocketPin);
 
-  // Get the sprocket and sensor values and calculate gradients
-
-  sprocketValue[nextData] = analogRead (sprocketPin);
-  sprocketGradient = sprocketValue[nextData] - sprocketValue[oldestData];
-
-  if (debugCount < DBG_CNT)
+  for (int j = nextGrad - NUM_VALS; j < nextGrad; j++)
     {
-      debugData[debugCount].current = sprocketValue[nextData];
-      debugData[debugCount].oldest = sprocketValue[oldestData];
-      debugData[debugCount].gradient = sprocketGradient;
-      debugCount++;
+      int prev = (j + NUM_SAMPLES) % NUM_SAMPLES;
+      int curr = (j + NUM_VALS) % NUM_SAMPLES;
+      tot += sprocketInfo.vals[curr] - sprocketInfo.vals[prev];
     }
-  else if (debugCount == DBG_CNT)
+
+  sprocketInfo.gradients[nextGrad] = tot;
+  sprocketInfo.gradient2prev = sprocketInfo.gradient2;
+  sprocketInfo.gradient2     = tot - sprocketInfo.gradients[prevGrad];
+
+  // Second derivative tells us when we crossed a sprocket edge. Because we
+  // may not get lucky with hitting zero, we look to see if we have changed
+  // sign.
+
+  // We look that we have a large enough first derivative to be sure this is a
+  // serious crossing, rather than just noise when there is no signal.
+
+  // We also can only enter if we are not already in and vice versa!
+
+  if ((sprocketInfo.gradient2 * sprocketInfo.gradient2prev) <= 0)
     {
-      Serial.println ("\"Current\",\"Oldest\",\"Gradient\"");
-
-      for (int i = 0; i < DBG_CNT; i++)
+      if (!sprocketState
+          && (sprocketInfo.gradients[nextGrad] < -SPROCKET_THRESHOLD))
         {
-          Serial.print ("\"");
-          Serial.print (debugData[i].current);
-          Serial.print ("\",\"");
-          Serial.print (debugData[i].oldest);
-          Serial.print ("\",\"");
-          Serial.print (debugData[i].gradient);
-          Serial.println ("\"");
-        }
+          // Entering a sprocket
 
-      debugCount++;
+          sprocketState = true;
+          //Serial.println ("In");
+
+          // If we have any data pin driving, accept it.
+
+          for (int i = 0; i < NUM_BITS; i++)
+            if (sensorState[i])
+              data |= 1 << i;
+        }
+      else if (sprocketState
+               && (sprocketInfo.gradients[nextGrad] > SPROCKET_THRESHOLD))
+        {
+          // Leaving a sprocket
+
+          sprocketState = false;
+          //Serial.println ("Out");
+
+          // Record the data
+
+          Serial.println (data, HEX);
+          data = 0;
+        }
     }
 
   for (int i = 0; i < NUM_BITS; i++)
     {
-      sensorValue[nextData][i] = analogRead (sensorPin[i]);
-      dataGradient[i] = sensorValue[nextData][i] - sensorValue[oldestData][i];
+      tot = 0;
+      dataInfo[i].vals[nextData] = analogRead (sensorPin[i]);
 
-      if (dataGradient[i] > THRESHOLD)
-        sensorState[i] = 1;
+      for (int j = nextGrad - NUM_VALS; j < nextGrad; j++)
+        {
+          int prev = (j + NUM_SAMPLES) % NUM_SAMPLES;
+          int curr = (j + NUM_VALS) % NUM_SAMPLES;
+          tot += dataInfo[i].vals[curr] - dataInfo[i].vals[prev];
+        }
 
-      if (dataGradient[i] < -THRESHOLD)
-        sensorState[i] = 0;
-    }
+      dataInfo[i].gradients[nextGrad] = tot;
+      dataInfo[i].gradient2prev = dataInfo[i].gradient2;
+      dataInfo[i].gradient2     = tot - dataInfo[i].gradients[prevGrad];
 
-  // For sprocket we need to capture the point of change.
+      // Recognize edges using second derivatives again.
 
-  if ((1 == sprocketState) && (sprocketGradient > THRESHOLD))
-    {
-      uint8_t dataVal = 0;
+      if ((dataInfo[i].gradient2 * dataInfo[i].gradient2prev) <= 0)
+        {
+          if (!sensorState[i]
+              && (dataInfo[i].gradients[nextGrad] < -SPROCKET_THRESHOLD))
+            {
+              // Entering a hole
 
-      digitalWrite (statusLedPin, HIGH);
+              sensorState[i] = true;
+              //Serial.print ("In ");
+              //Serial.println (i);
 
-      for (int i = 0; i < NUM_BITS; i++)
-        dataVal = dataVal << 1 | sensorState[i];
+              // If we are in a sprocket hole, mark this as data.
 
-      Serial.println (dataVal, HEX);
-    }
+              if (sprocketState)
+                data |= 1 << i;
 
-  // Update sprocketGradient info
+            }
+          else if (sensorState[i]
+                   && (dataInfo[i].gradients[nextGrad] > SPROCKET_THRESHOLD))
+            {
+              // Leaving a hole
 
-  if (sprocketGradient > THRESHOLD)
-    sprocketGradient = 1;
-
-  if (sprocketGradient < - THRESHOLD)
-    {
-      sprocketGradient = 0;
-      digitalWrite (statusLedPin, LOW);
+              sensorState[i] = false;
+              //Serial.print ("Out ");
+              //Serial.println (i);
+            }
+        }
     }
 
   // And ready for the next data
 
-  nextData = (nextData == (NUM_VALS - 1)) ? 0 : nextData + 1;
+  nextData = (nextData + 1) % NUM_SAMPLES;
 }
 
 
