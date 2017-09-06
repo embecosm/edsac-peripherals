472d1262339abb3983dada139d58a467c7dd7f25
maryEmbecosm <mary.bennett@embecosm.com>
2017-09-06 22:57:19 +0100

    Update simple_code.ino

--- delay-line/simple_code/simple_code.ino
+++ delay-line/simple_code/simple_code.ino
@@ -1,4 +1,15 @@
-/* The simple program that uss a 4kHz signal from the active buzzer
+/* TCopyright (C) 2017  Embecosm Ltd
+ * Contributor: Mary Bennett <mary.bennett@embecosm.com>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version. This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details. You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * **********************************************************
+ * The simple program that uss a 4kHz signal from the active buzzer
  * where; 0 = no signal, 1 = signal for 1 second
  * The first value recieved by the microphone is always 1.
  * ***********************************************************
@@ -7,26 +18,21 @@
  *   Decreased CLK for testing
  * *********************************************************** */
 
-// speaker pin: PD6
+// speaker pin: PD4 for cuttlefish
 
-const int SPEAKER_PIN = 6;
+const int SPEAKER_PIN = 4;
 
-// microphone pin: PD5
+// microphone pin: PD8 for cuttlefish
 
-const int MICROPHONE_PIN = 5;
+const int MICROPHONE_PIN = 8;
 
 // length of a clk pulse in ms
 
-const int CLK = 50;
-const int CLK_TWO = CLK/2;
-
-// threshold for average calculation
-
-const int THRESHOLD = 1;
+const int CLK = 1000;
 
 // amount of bits being sent through the tube
 
-const int LENGTH = 50;    //add 1, the first bit is always 1
+const int LENGTH = 36;
 
 // array of information being sent through the tube, first bit sent == 1 when recieved
 
@@ -35,10 +41,7 @@ const int WRITE [LENGTH] = {0, 1, 0, 1, 0, 0,
                             1, 1, 0, 1, 0, 0,
                             1, 1, 0, 0, 0, 1,
                             0, 0, 1, 0, 1, 0,
-                            0, 1, 1, 0, 1, 0,
-                            1, 1 ,1 ,0 ,1 ,0,
-                            0, 1, 1, 0, 0, 0,
-                            1, 0};
+                            0, 1, 1, 0, 1, 0};
 
 // which value in WRITE gets sent
 
@@ -46,7 +49,7 @@ int increment;
 
 // store samples used to calculate what the singal is
 
-int reading [3];
+int reading;
 
 // stores the information recieved from the buzzer
 
@@ -60,7 +63,7 @@ int mic_reading [LENGTH];
 void sendVal(int value) {
   if (value == 1) digitalWrite(SPEAKER_PIN, LOW);
   else if (value == 0) digitalWrite(SPEAKER_PIN, HIGH);
-  delay(CLK_TWO);
+  delay(CLK);
 }
 
 // initialise pins, serial ports and variables
@@ -71,7 +74,7 @@ void setup() {
 
   // initialise serial port
   Serial.begin(9600);
-  Serial.println("Start");
+  Serial.println("Start:");
 
   // initialise variables
   increment = 0;
@@ -79,8 +82,7 @@ void setup() {
   // wait half a second so that any remaining pulses/ reflections are gone
   delay(500);
 }
-
-// 
+ 
 void loop() {
   int reading_total = 0;
   
@@ -88,25 +90,21 @@ void loop() {
   sendVal(WRITE[increment]);
   
   // input: test for a 4kHz signal using 3 samples
-  for (int i = 0; i < 3; i++) {
-    reading[i] = digitalRead(MICROPHONE_PIN);
-  }
-  reading_total = reading[0] + reading[1] + reading[2];
-  if (reading_total > THRESHOLD) mic_reading[increment] = 0;  // higher than threshold
-  else mic_reading[increment] = 1;                            // lower
-
-  // so that the readings are in the middle of the pulse
-  delay(CLK_TWO);
+  reading = digitalRead(MICROPHONE_PIN);
+  
+  if (reading_total == 1) mic_reading[increment] = 1;  // higher than threshold
+  else mic_reading[increment] = 0;                            // lower
   
   increment++;
 
   // print to screen after each word is sent
   if (increment == LENGTH) {
     for(int i = 0; i < LENGTH; i++) {
-      if (i != 0) Serial.print(mic_reading[i]);
+      Serial.print(mic_reading[i]);
     }
     Serial.println();
     digitalWrite(SPEAKER_PIN, HIGH);
+    increment = 0;
     delay(CLK);
   }
 }
